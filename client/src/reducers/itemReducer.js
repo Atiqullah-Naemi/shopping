6fa13685f93cb7c236ba9833efa7edb09f9ff54f
atiqullah-naemi <atiqullah.naemi21@gmail.com>
2019-02-10 12:38:05 +1100

    connecting to backend

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -2,22 +2,22 @@ import uuid from 'uuid'
 import {
 	GET_ITEMS,
 	ADD_ITEM,
-	DELETE_ITEM
+	DELETE_ITEM,
+	ITEMS_LOADING
 } from '../actions/types'
 
 const initialState = {
-	items: [
-		{id: uuid(), name: 'Item 1'},
-		{id: uuid(), name: 'Item 2'},
-		{id: uuid(), name: 'Item 3'}
-	]
+	items: [],
+	loading: false
 }
 
 export default function(state = initialState, action) {
 	switch(action.type) {
 		case GET_ITEMS:
 			return {
-				...state
+				...state,
+				items: action.payload,
+				loading: false
 			}
 		case DELETE_ITEM:
 			return {
@@ -28,6 +28,11 @@ export default function(state = initialState, action) {
 			return {
 				...state,
 				items: [action.payload, ...state.items]
+			}
+		case ITEMS_LOADING:
+			return {
+				...state,
+				loading: true
 			}
 		default:
 			return state
